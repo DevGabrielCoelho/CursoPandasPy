2f8922434b0b09aa70cea6fec672f0a4f12b09b2
DevGabrielCoelho <gabriel.coelhosousasantos.pv@gmail.com>
2024-11-08 06:33:57 -0300

    estudos

---
+++ aula5/app.py
@@ -0,0 +1,32 @@
+""""""
+
+import pandas as pd
+import numpy as np
+from faker import Faker
+
+fake = Faker()
+
+indices_nome = []
+
+for i in range(32561):
+  indices_nome.append(fake.name())
+
+dataset = pd.read_csv("census.csv")
+
+serie_idade_nome = pd.Series(np.array(dataset['age']), index=indices_nome)
+
+print(serie_idade_nome.head(5))
+# print(serie_idade_nome["Clinton Choi"])
+
+serie_idade_nome_sem_duplicatas = serie_idade_nome.drop_duplicates()
+
+print(serie_idade_nome_sem_duplicatas.index)
+
+# print(serie_idade_nome_sem_duplicatas["Clinton Choi" : "Joseph Wright"])
+
+serie_idade_nome3 = serie_idade_nome_sem_duplicatas.copy()
+
+serie_idade_nome3 = serie_idade_nome3.reset_index(drop = True)
+
+print(serie_idade_nome3.head())
+print(serie_idade_nome3.index)
